744a1d5afdb660745909f521483f90802f627867
Anthony Perritano <aperritano@gmail.com>
2015-03-22 18:20:21 -0500

    MQTT intergration

---
+++ ios-mt-poster/AddPosterController.swift
@@ -0,0 +1,9 @@
+//
+//  AddPosterController.swift
+//  ios-mt-poster
+//
+//  Created by Anthony Perritano on 3/22/15.
+//  Copyright (c) 2015 LTG. All rights reserved.
+//
+
+import Foundation
---
+++ ios-mt-poster/AddUserController.swift
@@ -0,0 +1,9 @@
+//
+//  AddUserController.swift
+//  ios-mt-poster
+//
+//  Created by Anthony Perritano on 3/21/15.
+//  Copyright (c) 2015 LTG. All rights reserved.
+//
+
+import Foundation
